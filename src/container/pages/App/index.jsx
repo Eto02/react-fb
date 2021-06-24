61a40a06ca7821bbeca1295c17480f4d03b6cb5c
Eto02 <mmikoto89@gmail.com>
2021-06-25 00:14:55 +0700

    Redux Setting

--- src/container/pages/App/index.jsx
+++ src/container/pages/App/index.jsx
@@ -5,20 +5,45 @@ import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import Dashboard from '../Dashboard';
 import Login from '../Login';
 import Register from '../Register';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+ 
+const initialState={
+    popup:false,
+    isLogin:false
+}
+
+const reducer=(state=initialState,action)=>{
+    switch (action.type) {
+        case 'CHANGE_POPUP':
+            return {
+                ...state,
+                popup:action.value
+            }
+        case 'CHANGE_ISLOGIN':
+        return {
+            ...state,
+            isLogin:action.value
+        }
+        default:
+           return state
+    }
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <Router>
-      <div>
+    <Provider  store={store}>
+      <Router>
+        <div>
+            <Route path="/" exact>  <Dashboard/> </Route>
+            <Route path="/login">   <Login /> </Route>
+            <Route path="/register"> <Register /></Route>
       
-
-          <Route path="/" exact>  <Dashboard/> </Route>
-          <Route path="/login">   <Login /> </Route>
-          <Route path="/register"> <Register /></Route>
-         
-     
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
